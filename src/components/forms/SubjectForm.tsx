9b2964872e83935bb5e3277a3abfa42b631d0a12
SSEKITOLEKO MATHIUS (EA2231201010166) <sm0070@srmist.edu.in>
2025-06-08 13:07:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/forms/SubjectForm.tsx
+++ src/components/forms/SubjectForm.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import * from "react";
+import * as React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
