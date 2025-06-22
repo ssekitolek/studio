2c405f1bc4b7d5b7ba1384a05b6a014a4c7e5b0b
SSEKITOLEKO MATHIUS (EA2231201010166) <sm0070@srmist.edu.in>
2025-06-22 09:37:03 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1749365267352.cluster-jbb3mjctu5cbgsi6hwq6u4btwe.cloudworkstations.dev',
+    ],
+  },
+};
+
+module.exports = nextConfig;
