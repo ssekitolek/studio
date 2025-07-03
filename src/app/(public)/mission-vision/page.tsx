dab2eaf9ada1af82f36f4f8ecfe17b51707980d4
SSEKITOLEKO MATHIUS (EA2231201010166) <sm0070@srmist.edu.in>
2025-07-03 14:57:10 +0000

    i want the confusing file or the second file that should not be there to

--- src/app/(public)/mission-vision/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is being neutralized to resolve a routing conflict.
-// The active page is at /src/app/(marketing)/mission-vision/page.tsx.
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-// This file has been neutralized.
-// The homepage content has been moved to /src/app/(marketing)/page.tsx to be wrapped by the correct layout.
-export default function Page() {
-    return null;
-}
