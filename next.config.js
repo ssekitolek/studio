92027943ab535a7135dc1d09669dbaf6ca53ea6a
SSEKITOLEKO MATHIUS (EA2231201010166) <sm0070@srmist.edu.in>
2025-07-04 09:40:08 +0000

    make sure that the uploaded images are visible on the website

--- next.config.js
+++ next.config.js
@@ -39,6 +39,24 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   devIndicators: {
