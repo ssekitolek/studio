cf4824c71ef96177ef4910d9fcca380b36df2567
SSEKITOLEKO MATHIUS (EA2231201010166) <sm0070@srmist.edu.in>
2025-07-05 22:51:12 +0000

    still light grey is not visible can you change to a more visible color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,8 +44,7 @@ export default function RootLayout({
         {/* Next.js will automatically add the favicon link from the generated metadata. */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
         {children}
