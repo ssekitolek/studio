c03621eeee79a79f95bf69a4f038869beb6d91f1
SSEKITOLEKO MATHIUS (EA2231201010166) <sm0070@srmist.edu.in>
2025-07-06 00:00:10 +0000

    THE EFFECT ON THE WORD BEFORE THE DROP DOWN FOR EXAMPLE ABOU IS LIKE THA

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground data-[state=open]:text-destructive",
+        ghost: "hover:bg-accent hover:text-accent-foreground data-[state=open]:bg-destructive data-[state=open]:text-destructive-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
