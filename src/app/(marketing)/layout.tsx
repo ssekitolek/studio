2409bf735eaef3a768b0359d1cee800e51b3307b
SSEKITOLEKO MATHIUS (EA2231201010166) <sm0070@srmist.edu.in>
2025-07-06 08:10:00 +0000

    where there is st. mbaaga's college add the motto of the school Educatio

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -23,6 +23,7 @@ export default async function MarketingLayout({
             <div className="grid grid-cols-1 md:grid-cols-12 gap-8">
                 <div className="md:col-span-4">
                     <h4 className="font-headline font-semibold text-2xl">St. Mbaaga's College</h4>
+                    <p className="mt-1 text-primary-foreground/80 italic">Education for life</p>
                 </div>
 
                 <div className="md:col-span-2">
